6cfdbcb548c1dd7c9e8e0a970a4444944aefc2fd
eps1lon <silbermann.sebastian@gmail.com>
2025-02-13 21:29:21 +0100

    fixup! `turbopack://~project/` -> `turbopack:///[project]`

--- turbopack/crates/turbo-tasks-fs/src/lib.rs
+++ turbopack/crates/turbo-tasks-fs/src/lib.rs
@@ -1584,7 +1584,7 @@ impl ValueToString for FileSystemPath {
     #[turbo_tasks::function]
     async fn to_string(&self) -> Result<Vc<RcStr>> {
         Ok(Vc::cell(
-            format!("~{}/{}", self.fs.to_string().await?, self.path).into(),
+            format!("[{}]/{}", self.fs.to_string().await?, self.path).into(),
         ))
     }
 }
